873d75be1f84224ceb08d5ad9a664e30ac724fff
Jason (제이슨) <jasonjerome16@gmail.com>
2018-07-03 01:04:45 -0400

    optimized scripts

--- dir_authenticator.py
+++ dir_authenticator.py
@@ -2,6 +2,8 @@ import hashlib
 import sys
 import os
 
+BUFFER_SIZE = 65536  # 64kb chunks
+
 if len(sys.argv) != 3:
     print("ERROR: This script requires 3 total system arguments.")
     print("FORMAT: python <this_script.py> <src_dir> <dst_dir>")
@@ -19,36 +21,32 @@ if not os.path.isdir(dst_dir):
     print("Please make sure both directories exist!")
     sys.exit(1)
 
-src_dir_hash_list = []
-dst_dir_hash_list = []
 
-BUFFER_SIZE = 65536  # 64kb chunks
-
-for root, dirs, files in os.walk(src_dir, topdown=True):
-    for name in files:
-        fileName = os.path.join(root, name)
-        md5 = hashlib.md5()
-
-        with open(str(fileName), 'rb') as f:
-            data = f.read(BUFFER_SIZE)
-            if not data:
+def file_auth(file_name):
+    md5 = hashlib.md5()
+    with open(file_name, 'rb') as src:
+        while True:
+            data_src = src.read(BUFFER_SIZE)
+            if not data_src:
                 break
-            md5.update(data)
-        src_dir_hash_list.append(md5.hexdigest())
+            md5.update(data_src)
+    return md5.hexdigest()
 
-for root, dirs, files in os.walk(dst_dir, topdown=True):
-    for name in files:
-        fileName = os.path.join(root, name)
-        md5 = hashlib.md5()
 
-        with open(str(fileName), 'rb') as f:
-            data = f.read(BUFFER_SIZE)
-            if not data:
-                break
-            md5.update(data)
-        dst_dir_hash_list.append(md5.hexdigest())
+def dir_auth(dir_name):
+    hash_list = []
+    for root, dirs, files in os.walk(dir_name, topdown=True):
+        for name in files:
+            file_name = os.path.join(root, name)
+            hash_list.append(file_auth(file_name))
+    return hash_list
+
+
+src_dir_hash_list = dir_auth(src_dir)
+dst_dir_hash_list = dir_auth(dst_dir)
+
 
 if src_dir_hash_list == dst_dir_hash_list:
-    print("The provided directories are IDENTICAL")
+    print("The Src Directory and Dst Directory is IDENTICAL.")
 else:
-    print("The provided directories are DIFFERENT")
+    print("The Src Directory and Dst Directory is DIFFERENT.")
